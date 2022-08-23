560e1560c818169822055c9f8eecdcef9533d5c9
Bashra99 <telfahb@gmail.com>
2022-08-23 11:16:50 +0300

    new one

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -2,43 +2,32 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickNum: 0,
+    };
+  }
 
-class HornedBeast extends React.Component{
-  
-        constructor(props) {
-            super(props);
-            this.state = {
-                clickNum : 0
-            }
-        }
-    
-    
-        increaseNum = () => {
-            this.setState({
-                clickNum : this.state.voteNum + 1
-            })
-        }
-   
-    render() {
-        return(
-           
-            <Card style={{ width: '18rem' }} className="beast-card">
-            <Card.Body>
-              <Card.Title>{this.props.title}</Card.Title>
-            <Card.Img variant="top" src={this.props.img} onClick={this.increaseNum}/>
-              <Card.Text className="desc">
-              {this.props.description}
-              </Card.Text>
-              <Card.Text className="vote">
-              Votes
-              </Card.Text>
-              <Card.Text>
-              {this.state.onClick}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        )
-    }
+  increaseNum = () => {
+    this.setState({
+      clickNum: this.state.clickNum + 1,
+    });
+  };
+  render() {
+    return (
+      <Card style={{ width: "18rem" }} className="beast-card">
+        <Card.Img variant="top" src={this.props.img} onClick={this.increaseNum}/>
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text >{this.props.description}</Card.Text>
+          <Card.Text c>Votes : </Card.Text>
+          <Card.Text>{this.state.clickNum}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default HornedBeast;
--- src/component/Main .js
+++ src/component/Main .js
@@ -1,19 +1,23 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import cards from "./data";
+import data from "./data";
 
-let hornedData = cards.map((element) => (
-  <HornedBeast
-    key={element._id}
-    title={element.title}
-    img={element.image_url}
-    description={element.description}
-  />
-));
+
+let cardData = data.map(element => 
+    <HornedBeast 
+        key = {element._id}
+        title = {element.title}
+        img = {element.image_url}
+        description = {element.description}
+    /> 
+);
+
 class Main extends React.Component {
-  render() {
-    return hornedData;
-  }
+    render(){
+        return(
+            cardData
+        )
+    }
 }
 
 export default Main;
